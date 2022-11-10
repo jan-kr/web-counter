95c2caa5daa06d02dc76149103b18034f7851ef8
Jan Kressebuch <jan@kressebuch.net>
2022-11-10 13:56:36 +0100

    added code documentation

--- counter/js/script.js
+++ counter/js/script.js
@@ -1,23 +1,33 @@
 let apiurl = "https://counter.krsb.ch"
 let id = prompt("Enter your session code")
 
+/* Listens to the DOM content and executes setup after loading
+* finished.
+ */
 document.addEventListener("DOMContentLoaded", () => {
 	console.log("DOM content loaded.\nStarting setup...")
 	setup(id)
 })
 
+/* This method queries the api to set the counter value of the session id +1.
+ */
 function countUp(session) {
 	fetch(`${apiurl}/api/index.php?session=${session}&todo=add`)
 		.then(console.log("after up"))
 		.catch(`call failed for ${session}`)
 }
 
+/* This method queries the api to set the counter value of the session id -1.
+ */
 function countDown(session) {
 	fetch(`${apiurl}/api/index.php?session=${session}&todo=del`)
 		.then(console.log("after down"))
 		.catch(`call failed for ${session}`)
 }
 
+/* This method queries the api for the current counter value for the given
+ * session id in the database.
+ */
 function getCount(session) {
 	fetch(`${apiurl}/api/index.php?session=${session}&todo=get`)
 		.then(res => res.json())
@@ -25,6 +35,10 @@ function getCount(session) {
 		.catch(`call failed for ${session}`)
 }
 
+/* (Deprecated: use own session identifier instead)
+ * This method returns the session id of the PHP session at the backend
+ * this can be used to connect displays and counters to the same session.
+ */
 function getSession() {
 	fetch(`${apiurl}/api/index.php?todo=session`)
 		.then(res => res.json())
@@ -32,12 +46,19 @@ function getSession() {
 		.catch(`call failed`)
 }
 
+/* This method builds the session and makes an api call to create the database
+ * entry if it is a new session.
+ */
 function buildSession(session) {
 	fetch(`${apiurl}/api/index.php?session=${session}&todo=connect`)
 		.then("Session built.")
 		.catch(`call failed for ${session}`)
 }
 
+/* This method is used to initialize the session for the given session id
+ * It adds the event listeners to the object and starts the refresh
+ * interval of the display.
+ */
 function setup(session) {
 	console.log(`Started setup...\nBuild session ${session}...`)
 	buildSession(session)
